00d8944e149668760025253ea91b7a7c98810080
AJane <dbliuchengjie@163.com>
2018-12-02 21:36:29 +0800

    地址选择 过渡动画

--- src/view/home.vue
+++ src/view/home.vue
@@ -6,13 +6,20 @@
             <label>所在地区:</label>
             <input readonly type="text" :placeholder="selecedCity?(myAddressProvince+myAddressCity+myAddresscounty):'地址'" />
         </div>
-        <div class="picker-city" v-if="showCityPicker">
-            <div class="menu-box">
-                <div>取消</div>
-                <div @click="confirmCityData">确定</div>
+        <transition name="fade">
+            <div class="picker-city-box" v-if="showCityPicker"></div>
+        </transition>
+        <transition name="slide">
+            <!-- <div class="picker-city-box" v-if="showCityPicker"> -->
+            <div class="picker-city" v-if="showCityPicker">
+                <div class="menu-box">
+                    <div @click="cancelCityData">取消</div>
+                    <div @click="confirmCityData">确定</div>
+                </div>
+                <mt-picker :slots="myAddressSlots" @change="onMyAddressChange"></mt-picker>
             </div>
-            <mt-picker :slots="myAddressSlots" @change="onMyAddressChange"></mt-picker>
-        </div>
+            <!-- </div> -->
+        </transition>
     </div>
 </template>
 <script>
@@ -27,7 +34,7 @@
                 tempAddressProvince: '',
                 tempAddressCity: '',
                 tempAddresscounty: '',
-                showCityPicker:false,
+                showCityPicker: false,
                 myAddressSlots: [{
                         flex: 1,
                         defaultIndex: 0,
@@ -36,7 +43,7 @@
                         textAlign: 'center'
                     }, {
                         divider: true,
-                        content: '-',
+                        content: '',
                         className: 'slot2'
                     }, {
                         flex: 1,
@@ -46,7 +53,7 @@
                     },
                     {
                         divider: true,
-                        content: '-',
+                        content: '',
                         className: 'slot4'
                     },
                     {
@@ -58,11 +65,11 @@
                 ]
             };
         },
-        computed:{
-            selecedCity(){
-                return this.myAddressProvince&&this.myAddressCity&&this.myAddresscounty ? true :false;
+        computed: {
+            selecedCity() {
+                return this.myAddressProvince && this.myAddressCity && this.myAddresscounty ? true : false;
             }
-        
+
         },
         mounted() {},
         methods: {
@@ -70,18 +77,21 @@
                 if (myaddress[values[0]]) { //这个判断类似于v-if的效果（可以不加，但是vue会报错，很不爽）
                     picker.setSlotValues(1, Object.keys(myaddress[values[0]])); // Object.keys()会返回一个数组，当前省的数组
                     picker.setSlotValues(2, myaddress[values[0]][values[1]]); // 区/县数据就是一个数组
-                    this.tempAddressProvince = values[0]+' ';
-                    this.tempAddressCity = values[1]+' ';
+                    this.tempAddressProvince = values[0] + ' ';
+                    this.tempAddressCity = values[1] + ' ';
                     this.tempAddresscounty = values[2] + ' ';
                 }
             },
-            cityPicker(){
+            cityPicker() {
                 this.showCityPicker = true;
             },
             confirmCityData() {
                 this.myAddressProvince = this.tempAddressProvince;
                 this.myAddressCity = this.tempAddressCity;
                 this.myAddresscounty = this.tempAddresscounty;
+                this.showCityPicker = false;
+            },
+            cancelCityData() {
                 this.showCityPicker = false;
             },
         }
@@ -99,16 +109,76 @@
 
     .menu-box {
         width: 100%;
+        box-sizing: border-box;
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
+        padding: 0 20px;
+    }
+
+    .menu-box div {
+        color: blue;
+    }
+
+    .picker-city-box {
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.4);
+        position: absolute;
+        top: 0;
+        left: 0;
     }
 
     .picker-city {
         position: absolute;
+        background: #ffffff;
         bottom: 0;
         left: 0;
         width: 100%;
+    }
+
+    .picker-selected {
+        border-top: 1px solid #000000;
+        border-bottom: 1px solid #000000;
+    }
+
+    /* 可以设置不同的进入和离开动画 */
+    /* 设置持续时间和动画函数 */
+    .fade-enter-active {
+        transition: all .3s ease;
+    }
+
+    .fade-leave-active {
+        transition: all .3s ease-in;
+    }
+
+    .fade-enter,
+    .fade-leave-to
+
+    /* .fade-leave-active for below version 2.1.8 */
+        {
+        opacity: 0;
+    }
+
+    .slide-enter-active {
+        animation: slide .3s;
+    }
+
+    .slide-leave-active {
+        animation: slide .3s reverse;
+    }
+
+    @keyframes slide {
+        0% {
+            /* top: calc(100%); */
+            bottom: -400px;
+        }
+
+
+
+        100% {
+            bottom: 0;
+        }
     }
 </style>
